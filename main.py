f0f47158168b36c44b299bb9cf1c56f3df78c498
JonathanCadiz <jonathancadiz0@gmail.com>
2021-02-01 10:02:16 -0500

    Added language_processing.py to perform basic analysis on the text from comments. - Added extract keyword function to use NLP to determine what words are nouns, adjectives, and verbs. - Exclude words that are in a predefined list or a user-inputted without list. - Removed links from words, but added link_list to still perform analysis on links. - Used Pandas to get a count of the prevalence of each word. - Used RegEx to help clean some of the data. - TODO: Refactor code, create a Language Process and Analysis class, or something like that.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import comments
+import language_processing as lp
 
 
 def main():
     new_request = comments.VideoRequest(input("Please enter your video's URL\n"))
     new_request.request_comments()
+    text_list = []
     for comment in new_request.all_comments:
         print(comment.text)
+        text_list.append(comment.text)
     print(len(new_request.all_comments))
+    text_string = ".".join(text_list)
+    print(text_string)
+    print(lp.extract_keywords(text_string))
 
 
 if __name__ == "__main__":
