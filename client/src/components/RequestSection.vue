2443bc12af9695ba767bcf110b3a2e39c55e9805
JonathanCadiz <jonathancadiz0@gmail.com>
2021-02-07 05:50:10 -0500

    Added title and request section to vue front end.

--- client/src/components/RequestSection.vue
+++ client/src/components/RequestSection.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
+    <title-text id="title"></title-text>
     <video-input id="videoInput"></video-input>
   </div>
 </template>
 
 <script>
+import TitleText from './Title.vue';
 import VideoInput from './VideoInput.vue';
 
 export default {
-  components: { VideoInput },
+  components: { VideoInput, TitleText },
   name: 'RequestSection',
 };
 </script>
@@ -16,6 +18,8 @@ export default {
 <style scoped>
   #videoInput {
     width: 65%;
+    margin-top: 60px;
+    margin-bottom: 60px;
   }
 
   .container {
